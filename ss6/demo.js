bed2388c36f1189538489abcf81b72bf27682bb7
haitan2408 <haitan28102408@gmail.com>
2025-02-10 21:21:42 +0700

    up demo ss6

---
+++ ss6/demo.js
@@ -0,0 +1,6 @@
+function changeBack() {
+    let likeBtn = document.getElementById("like-btn");
+    likeBtn.style.color="blue";
+    likeBtn.innerText="Đã thích";
+    likeBtn.style.width="100px";
+}
